f1ed2115c82644787a7757e0da6b49b8bc92401d
pearmai <111360164+moodmoon@users.noreply.github.com>
2024-04-05 02:24:52 +0700

    Delete src directory

--- src/main/java/sit/int204/testclassicmodelsservice/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package sit.int204.testclassicmodelsservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int204.testclassicmodelsservice.entities.Office;
-
-public interface EmployeeRepository extends JpaRepository<Office,Integer> {
-}
--- src/main/java/sit/int204/testclassicmodelsservice/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package sit.int204.testclassicmodelsservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int204.testclassicmodelsservice.entities.Order;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-}
